ecd37143bf1cba378ea7e1e81c8fb298d2e2835a
espacoculturaemail-source <espacoculturaemail@gmail.com>
2026-04-19 15:32:25 -0300

    Add basic App component with welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>🔥 Seu app está funcionando</h1>
+      <p>Agora sim você está online!</p>
+    </div>
+  );
+}
 
+export default App;
